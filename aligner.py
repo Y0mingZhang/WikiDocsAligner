04262b3664fed3589f00d9cb44d5884fe45ad2ba
Yiming Zhang <yimingz@lit100.eecs.umich.edu>
2020-02-11 15:21:08 +0000

    aligner fix (just use langlinks)

--- aligner.py
+++ aligner.py
@@ -4,41 +4,31 @@ import parse_sql_script
 import os
 import worker
 import shutil
+import pickle
 
-
-def main(src_lang, target_lang, src_ll_sql, src_corpus_dir, target_corpus_dir, out_dir, overwrite=False):
+def main(src_lang, target_lang, src_ll_sql, out_dir, overwrite=False):
     print('source language:', src_lang)
     print('target language:', target_lang)
     print('source language links sql file', src_ll_sql)
-    print('source corpus directory', src_corpus_dir)
-    print('target corpus directory', target_corpus_dir)
     print('output directory', out_dir)
     print('Force overwrite', args.overwrite)
     
+    df_path = os.path.join(output_dir, '{}_{}_langlinks'.format(src_lang, target_lang))
 
-    src_path = os.path.join(out_dir, src_lang)
-    target_path = os.path.join(out_dir, target_lang)
-    if (os.path.exists(src_path) or os.path.exists(target_path)):
+    if os.path.exists(df_path):
         if overwrite:
             shutil.rmtree(out_dir)
         else:
             raise ValueError
 
-    if not os.path.exists(src_path): os.makedirs(src_path)
-    if not os.path.exists(target_path): os.makedirs(target_path)
+    if not os.path.exists(out_dir): 
+        os.makedirs(out_dir)
+
 
     src_df = parse_sql_script.sql2df(src_ll_sql, target_lang)
 
     print("inter-language links sql file loaded successfully")
-
-
-
-    src_corpus = worker.load_corpus(src_corpus_dir, mode='id', arg=set(src_df.ll_from))
-    target_corpus = worker.load_corpus(target_corpus_dir, mode='title', arg=set(src_df.ll_title))
-    print("source and target corpus loaded successfully")
-    worker.do_work(src_lang, target_lang, src_df, src_corpus, target_corpus, out_dir)
-
-
+    pickle.dump(src_df, open(df_path, 'wb'), protocol=pickle.HIGHEST_PROTOCOL)
 
 
 
@@ -48,29 +38,21 @@ parser.add_argument('--src-lang', type=str, help='source language. '
                                                 'e.g., ar for Arabic, '
                                                 'en for English, or '
                                                 'fr for French ...', required=True)
-parser.add_argument('--target-lang', type=str, help='target language. '
+parser.add_argument('--tgt-lang', type=str, help='target language. '
                                                    'e.g., ar for Arabic, '
                                                    'en for English, or '
                                                    'fr for French ...', required=True)
 parser.add_argument('--sql-file', type=str, help='source language links sql file. '
                                                 'Obtained from https://dumps.wikimedia.org/', required=True)
-parser.add_argument('--src-corpus', type=str, help='source corpus directory.', required=True)
-parser.add_argument('--target-corpus', type=str, help='target corpus directory.', required=True)
 parser.add_argument('--out-dir', type=str, help='the output directory.', required=True)
-parser.add_argument('--overwrite', type=bool, action='store_true')
+parser.add_argument('--overwrite', action='store_true')
 
 if __name__ == '__main__':
     args = parser.parse_args()
     src_lang = args.src_lang
-    target_lang = args.target_lang
+    target_lang = args.tgt_lang
     src_ll_sql = args.sql_file
-    src_corpus_dir = args.src_corpus
-    target_corpus_dir = args.target_corpus
     out_dir = args.out_dir
-    main(src_lang, target_lang, src_ll_sql, src_corpus_dir, target_corpus_dir, out_dir, args.overwrite)
+    main(src_lang, target_lang, src_ll_sql, out_dir, args.overwrite)
 
-'''
 
-python aligner.py ar arz /home/motaz/back09022017/wiki/arwiki-20170120-langlinks.sql /home/motaz/back09022017/wiki/arwiki /home/motaz/back09022017/wiki/arzwiki /home/motaz/tmp/out/
-
-'''
